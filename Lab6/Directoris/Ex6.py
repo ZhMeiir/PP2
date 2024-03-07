4eaf19ca4f82ded13a936b153b34b69d9c9875db
ZhMeiir <156984919+ZhMeiir@users.noreply.github.com>
2024-03-07 14:47:28 +0500

    Ex6.py

--- Lab6/Directoris/Ex6.py
+++ Lab6/Directoris/Ex6.py
@@ -1,12 +1,5 @@
-import string
-
-def generate_files():
-    for letter in string.ascii_uppercase:
-        with open(f"{letter}.txt", "w") as file:
-            file.write(f"{letter}.txt")
-
-def main():
-    generate_files()
-
-if __name__ == "__main__":
-    main()
+let = ord('A')
+for i in range(1, 27):
+    file = open(f"dirFile\\task6_alphabet\\{chr(let)}.txt", "x")
+    let += 1
+    file.close()
